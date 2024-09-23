0c1dc56f7b6dfd7466a15cbbf9bbd9e796a53121
dana <157533000+Dana11a@users.noreply.github.com>
2024-09-23 14:49:17 +0300

    git commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    
+        List<String> students= new ArrayList<>();
+        students.add("Danah");
+        students.add("Raghadah");
+
     }
 }
